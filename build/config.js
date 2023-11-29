0c3941ac2f01002b9c59550ece90d66e69452001
abdulhaseebj <abdulhaseeb4326@gmail.com>
2023-11-28 22:29:22 -0800

    changing in signup form

---
+++ build/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApm7FW3j43iZjfDiUOkko_VQTqOqCC5lE",
+    authDomain: "blog-app12.firebaseapp.com",
+    projectId: "blog-app12",
+    storageBucket: "blog-app12.appspot.com",
+    messagingSenderId: "590207172725",
+    appId: "1:590207172725:web:96bc87994f6d161ca1f83c"
+  };
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
